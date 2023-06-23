c627f0085406d419054abefc9b0be2ddf95eba7c
jash-j3 <jash.jhatakia@gmail.com>
2023-06-24 02:06:28 +0530

    Styled the login page

--- src/mainApp/pages/login.jsx
+++ src/mainApp/pages/login.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import './login.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import Footer from '../components/Footer'
 
 function Login() {
     const navigate = useNavigate();
@@ -37,27 +41,53 @@ function Login() {
       }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type={"email"}
-          name="email"
-          id="email"
-          value={data.email}
-          onChange={onChange}
-        />
-        <label htmlFor="pass">Password</label>
-        <input
-          type={"password"}
-          name="pass"
-          id="pass"
-          value={data.pass}
-          onChange={onChange}
-        />
-        <button>Submit</button>
-      </form>
-      <Link to= '/signup'>
-      <button>New? Signup</button></Link>
+      <div class="container-sgnp">
+            <form onSubmit={handleSubmit}>
+        <div class="login-box">
+          <div class="login-content">
+            <h1>Login</h1>
+
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </i>
+                <label htmlFor="email">
+                  <h2>Email ID</h2>
+                </label>
+                  <input
+                    type={"email"}
+                    name="email"
+                    id="email"
+                    value={data.email}
+                    onChange={onChange}
+                    placeholder="ilove@jcubestore.com"
+                  />
+              </div>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faLock} />
+                </i>
+                <label htmlFor="pass">
+                  <h2>Password</h2>
+                </label>
+                <input
+                  type={"password"}
+                  name="pass"
+                  id="pass"
+                  value={data.pass}
+                  onChange={onChange}
+                  placeholder="Recall the Strong One!"
+                />
+              </div>
+              <h3>
+                New? Signup <Link to='/signup' className="linktologin">Here</Link>
+              </h3>
+              <button>SIGN UP</button>
+          </div>
+        </div>
+            </form>
+      </div>
+      <Footer />
     </div>
   )
 }
