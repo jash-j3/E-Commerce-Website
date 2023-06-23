dbe0275a624e72a38a091d51ed662e9288cfe399
jash-j3 <jash.jhatakia@gmail.com>
2023-06-24 01:56:06 +0530

    Styled the signup page

--- src/mainApp/pages/signup.jsx
+++ src/mainApp/pages/signup.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import "./signup.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
+import Footer from '../components/Footer'
 
 function Signup() {
+  const element = <FontAwesomeIcon icon={faUser} />;
   const navigate = useNavigate();
   const [data, setData] = useState({
     fName: "",
@@ -31,58 +35,107 @@ function Signup() {
         const dataRes = await fetchData.json();
         console.log(dataRes);
         if (dataRes.alert) {
-          navigate('/login');
+          navigate("/login");
         }
       }
     }
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="fName">First Name</label>
-        <input
-          type={"text"}
-          name="fName"
-          id="Name"
-          value={data.fName}
-          onChange={onChange}
-        />
-        <label htmlFor="lName">Last Name</label>
-        <input
-          type={"text"}
-          name="lName"
-          id="lName"
-          value={data.lName}
-          onChange={onChange}
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type={"email"}
-          name="email"
-          id="email"
-          value={data.email}
-          onChange={onChange}
-        />
-        <label htmlFor="pass">Password</label>
-        <input
-          type={"password"}
-          name="pass"
-          id="pass"
-          value={data.pass}
-          onChange={onChange}
-        />
-        <label htmlFor="cpass">Confirm Password</label>
-        <input
-          type={"password"}
-          name="cpass"
-          id="cpass"
-          value={data.cpass}
-          onChange={onChange}
-        />
-        <button>Submit</button>
-      </form>
-      <Link to= '/login'>
-      <button>Already joined our fam? Login</button></Link>
+      <div class="container-sgnp">
+            <form onSubmit={handleSubmit}>
+        <div class="login-box">
+          <div class="login-content">
+            <h1>Sign Up</h1>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faUser} />
+                </i>
+                <label htmlFor="fName">
+                  <h2>First Name</h2>
+                </label>
+                <input
+                  type={"text"}
+                  name="fName"
+                  id="fname"
+                  value={data.fName}
+                  onChange={onChange}
+                  placeholder="We'll call you by that!"
+                />
+              </div>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faUser} />
+                </i>
+                <label htmlFor="lName">
+                  <h2>Last Name</h2>
+                </label>
+                <input
+                  type={"text"}
+                  name="lName"
+                  id="lName"
+                  value={data.lName}
+                  onChange={onChange}
+                  placeholder="Ufff, too many formalities?"
+                />
+              </div>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </i>
+                <label htmlFor="email">
+                  <h2>Email ID</h2>
+                </label>
+                  <input
+                    type={"email"}
+                    name="email"
+                    id="email"
+                    value={data.email}
+                    onChange={onChange}
+                    placeholder="Won't spam, Don't Worry!!"
+                  />
+              </div>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faLock} />
+                </i>
+                <label htmlFor="pass">
+                  <h2>Password</h2>
+                </label>
+                <input
+                  type={"password"}
+                  name="pass"
+                  id="pass"
+                  value={data.pass}
+                  onChange={onChange}
+                  placeholder="Choose a Strong One!"
+                />
+              </div>
+              <div class="wrap">
+                <i>
+                  <FontAwesomeIcon icon={faLock} />
+                </i>
+                <label htmlFor="cpass">
+                  <h2>Confirm Password</h2>
+                </label>
+                <input
+                    type={"password"}
+                    name="cpass"
+                    id="cpass"
+                    value={data.cpass}
+                    onChange={onChange}
+                    placeholder="Make it Match the Strong One!"
+                  />
+              </div>
+              <h3>
+                Already a User? Login <Link to='/login' className="linktologin">Here</Link>
+              </h3>
+              <button>LOGIN</button>
+          </div>
+        </div>
+            </form>
+      </div>
+      <Footer />
     </div>
   );
 }
